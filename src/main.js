81c7343c764e55c2de2f57aa404cfbf55ed8ffc0
Mehul <jadavmehul555@gmail.com>
2024-12-05 13:17:16 +0530

    added custom delimiter support

--- src/main.js
+++ src/main.js
@@ -1,13 +1,36 @@
-export default function add(inputString) {
+export default function add(input) {
+    let inputString = input;
+
     if(typeof inputString !== 'string') {
         // we will not support String objects as wells
         throw new Error('Unsupported data type, expected a string')
     }
 
+    let delimiterPattern = /[\n,]/; // Defaults to comma and newline
+
+    if (inputString.startsWith("//")) {
+        const endOfDelimiter = inputString.indexOf("\n");
+        const customDelimiter = inputString.slice(2, endOfDelimiter);
+        delimiterPattern = new RegExp(`[${customDelimiter}]`);
+        inputString = inputString.slice(endOfDelimiter + 1); // Remove the custom delimiter line
+      }
+    
+
     // either split by new line or ,
-    const numbers = inputString.split(/\n|,/).map(i => Number(i))
+    const numbers = inputString.split(delimiterPattern).map(i => Number(i))
+
+    // filter for negative numbers
+    const negativeList = numbers.filter(i => !isNaN(i) && i < 0)
+    if(negativeList.length > 0) {
+        throw new Error(`negatives not allowed, contains ${negativeList.join(',')}`)
+    }
 
     return numbers.reduce((sum, item) => {
+        // Numbers bigger than 1000 should be ignored, so adding 2 + 1001 = 2
+        if(item > 1000) {
+            return sum;
+        }
+
         if(!isNaN(item)) {
             sum += item
         }
--- src/main.test.js
+++ src/main.test.js
@@ -1,11 +1,11 @@
 import sum from './main'
 
 describe('Sum', () => {
-    test('returns 0 for undefined or empty string', () => {
+    test('for undefined or empty string', () => {
         expect(sum('')).toBe(0)
     })
 
-    test('single number in input string, returns a number', () => {
+    test('when single number in input string, returns a number', () => {
         expect(sum("1")).toBe(1);
 
         // test 0 as it is falsy value
@@ -27,4 +27,21 @@ describe('Sum', () => {
     test('returns sum of numbers when input string contains new line', () => {
         expect(sum('1\n2,3')).toBe(6)
     })
+    test('when input string contains has custom delimiter', () => {
+        expect(sum('//;\n1;2')).toBe(3)
+
+        // extra - supports any length for custom delimiter
+        expect(sum('//[***]\n1***2***3')).toBe(0)
+    })
+
+    test('throws an error when numbers are negative', () => {
+        expect(() => sum('-1,-2,-3')).toThrow(
+            'negatives not allowed, contains -1,-2,-3'
+        )
+    })
+
+    test('ignores numbers greater than 1000', () => {
+        expect(sum('1001,2,3000')).toBe(2)
+    })
+
 })
