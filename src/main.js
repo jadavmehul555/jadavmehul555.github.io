8db95fa493e9758f2d04e46ef82c19e9e1c488d8
Mehul <jadavmehul555@gmail.com>
2024-12-05 12:32:32 +0530

    Setup boilerplate code and base test case

---
+++ src/main.js
@@ -0,0 +1,3 @@
+export default function add(inputString) {
+    return 0
+}
---
+++ src/main.test.js
@@ -0,0 +1,7 @@
+import sum from './main'
+
+describe('Sum', () => {
+    test('returns a test string', () => {
+        expect(sum()).toBe(0)
+    })
+})
