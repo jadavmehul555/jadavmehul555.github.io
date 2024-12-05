057c5a911746945f828692054588f58ca29cb543
Mehul <jadavmehul555@gmail.com>
2024-12-05 12:47:48 +0530

    throw error for unsupported data type

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 export default function add(inputString) {
+    if(typeof inputString !== 'string') {
+        // we will not support String objects as wells
+        throw new Error('Unsupported data type, expected a string')
+    }
 
     const number = Number(inputString)
     if(!isNaN(number)) {
--- src/main.test.js
+++ src/main.test.js
@@ -2,7 +2,6 @@ import sum from './main'
 
 describe('Sum', () => {
     test('returns 0 for undefined or empty string', () => {
-        expect(sum()).toBe(0)
         expect(sum('')).toBe(0)
     })
 
@@ -11,5 +10,12 @@ describe('Sum', () => {
 
         // test 0 as it is falsy value
         expect(sum('0')).toBe(0);
+    })
+
+    test('throws error when input is not a string', () => {
+        const message = 'Unsupported data type, expected a string'
+        expect(() => sum()).toThrow(message)
+        expect(() => sum({})).toThrow(message);
+        expect(() => sum(false)).toThrow(message)
     })
 })
