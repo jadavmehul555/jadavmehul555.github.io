4c8d13f18769a1a76027e137ccd9e69399d20932
Mehul <jadavmehul555@gmail.com>
2024-12-05 12:39:05 +0530

    support for single number

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 export default function add(inputString) {
+
+    const number = Number(inputString)
+    if(!isNaN(number)) {
+        return number
+    }
+
     return 0
 }
--- src/main.test.js
+++ src/main.test.js
@@ -1,7 +1,15 @@
 import sum from './main'
 
 describe('Sum', () => {
-    test('returns a test string', () => {
+    test('returns 0 for undefined or empty string', () => {
         expect(sum()).toBe(0)
+        expect(sum('')).toBe(0)
+    })
+
+    test('single number in input string, returns a number', () => {
+        expect(sum("1")).toBe(1);
+
+        // test 0 as it is falsy value
+        expect(sum('0')).toBe(0);
     })
 })
